42ae0fde296d98276000277d2b45790f3df57abd
kami-S06 <camilledarbois2@gmail.com>
2024-10-17 13:27:45 +0200

    Creating maze.py and maze.txt

---
+++ maze.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+class StackFrontier:
+    def __init__(self):
+        self.frontier = []
+    def add(self, node):
+        self.frontier.append(node)
+    def empty(self):
+        return len(self.frontier) == 0
+    def remove(self):
+        if self.empty():
+            print("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier.remove(node)
+            return node
+class Maze:
+    def __init__(self):
+        with open(Untilted-1) as f:
+            contents = f.read()
+        if contents.count("A") != 1:
+            raise Exception("Problem with A")
+        if contents.count("B") != 1:
+            raise Exception("Problem with B")
+        
+        
+
+node = Node()
